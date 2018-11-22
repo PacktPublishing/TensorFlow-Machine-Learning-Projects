143e3ddd5fa7b7a4dc2b95419106435ba7ffa2e2
Dr Amita Kapoor <kapooramita@gmail.com>
2018-11-22 17:07:44 +0530

    Update tensorflow_distributed_dl.py

--- Chapter12/tensorflow_distributed_dl.py
+++ Chapter12/tensorflow_distributed_dl.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 # Add other module libraries you may need
 
 cluster = tf.train.ClusterSpec(
-          {'ps':['192.168.1.4:2222'],
-           'worker': ['192.168.1.4:2222']
+          {'ps':['192.168.1.3:2222'],
+           'worker': ['192.168.1.4:2222',
+                      '192.168.1.5:2222',
+                      '192.168.1.6:2222',
+                      '192.168.1.7:2222']
  })
 
 job = sys.argv[1]
